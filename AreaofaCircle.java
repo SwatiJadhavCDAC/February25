95214d3dc42beb6a899c3efd167fdf6c43ec6449
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-17 18:01:14 +0530

    Add files via upload

---
+++ AreaofaCircle.java
@@ -0,0 +1,13 @@
+/*9. Calculate the Area of a Circle*/
+
+class AreaofaCircle{
+	public static void main (String args[]){
+	double radius =7;
+	double pi = 3.14159;
+	double area = pi*radius*radius;
+
+	
+	System.out.println(area);
+
+	}
+}
