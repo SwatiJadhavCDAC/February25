930ab1b5528ca0ebbab702b48869f0701fb698d4
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-17 17:12:39 +0530

    Add files via upload

---
+++ BAO.java
@@ -0,0 +1,17 @@
+/*6. Basic Arithmetic Operations*/
+
+class BAO{
+	public static void main (String args[]){
+	int a = 125;
+	int b = 24;
+	int c = a+b;
+	int d = a-b;
+	int e = a*b;
+	int f = a/b;
+	int g = a%b;
+
+
+	System.out.println(c+","+d+","+e+","+f+","+g );
+
+	}
+}
