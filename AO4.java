1d9620b9f5e0e9772b811e0872e178002a9c07d8
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-17 16:24:13 +0530

    Add files via upload

---
+++ AO4.java
@@ -0,0 +1,12 @@
+class AO4{
+	public static void main(String args[]){
+	byte a = 5;
+	byte b = 15;
+	byte c = 3;
+	byte d = 2;
+	byte e = 8;
+	byte f = (byte)(a+b/c*d-e%c);
+
+	System.out.println (f);
+}
+}
