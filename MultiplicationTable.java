fcb16338dc8d9d67c61e00623dbf272cee3ad5a6
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-18 09:19:24 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,12 @@
+/*7. Multiplication Table*/
+
+class MultiplicationTable{
+    public static void main(String[] args) {
+        
+        int number = 8;  
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println(number+"X"+i+"="+(number * i));
+        }
+    }
+}
