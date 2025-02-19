70bf75718ced05c798d03b88fb2becc9851ca337
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-19 15:06:05 +0530

    Add files via upload

---
+++ AsciiExample.java
@@ -0,0 +1,7 @@
+public class AsciiExample {
+    public static void main(String[] args) {
+        char letter = 'A';  // Character
+        int Swati = (int) letter;  // Casting char to int gives ASCII value
+        System.out.println("The ASCII value of 'A' is: " + Swati);
+    }
+}
