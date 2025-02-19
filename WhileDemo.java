812f584fab1e975f7f37a0f45074d7b22787bca6
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-19 15:08:16 +0530

    Add files via upload

---
+++ WhileDemo.java
@@ -0,0 +1,19 @@
+class WhileDemo{
+	public static void main(String args[]){
+	int a=10;
+
+while(false){
+System.out.println(a);
+a--;
+}
+
+/*
+while(true){
+System.out.println(a);
+a--;
+}
+*/
+	System.out.println("Outside while");
+
+}
+}
