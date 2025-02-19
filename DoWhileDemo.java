70de515d31c5090f36d32ed78371d824cd8495f9
SwatiJadhavCDAC <swatijadhav10821@gmail.com>
2025-02-19 15:06:45 +0530

    Add files via upload

---
+++ DoWhileDemo.java
@@ -0,0 +1,32 @@
+class DoWhileDemo{
+
+public static void main(String args[]){
+
+int a = 10;
+do{
+
+System.out.println("DoWhileDemo");
+a--;
+}while(a>=5);
+
+
+
+System.out.println("Outside DoWhile");
+
+
+
+
+/*
+do{
+System.out.println("DoWhileDemo");
+}while(false);
+
+
+
+do{
+System.out.println("DoWhileDemo");
+}while(false)
+System.out.println("Outside DoWhile");
+*/
+}
+}
